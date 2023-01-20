37947a6699b276dbdcea43b7effbc502df0198bb
c-dacanay <tina.dacanay@gmail.com>
2023-01-20 09:02:00 -0500

    js reference

---
+++ cheatsheet.js
@@ -0,0 +1,7 @@
+
+//all colors, first two are white and black
+let colors = ['#FFFFF1','#292B2C', '#E298E4', '#2e94b0', '#7B67F3', '#B6FF8E', '#FBFF7B','#FA5F66']
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+    }
