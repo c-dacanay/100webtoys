0c65fadb4cff70a4e363bf174c07e7b8f019bb76
c-dacanay <tina.dacanay@gmail.com>
2023-04-01 20:46:55 -0400

    90

--- 90/script.js
+++ 90/script.js
@@ -3,6 +3,7 @@ const images = document.querySelectorAll(".hexagon img")
 const wrapper = document.getElementById("wrapper")
 const content = document.getElementById("content")
 const title = document.querySelector("#content h1")
+const poem = document.querySelector("#content p")
 const bigimg = document.querySelector("#content img")
 const readings = {
   conq: {
@@ -19,15 +20,15 @@ const readings = {
   },
   heretic: {
     "title": "The Heretic",
-    "poem": "<p>The smell of a tortoise boiling<br>It cries under the cauldron's lid</p><p>Surrender yourself to smallness<br>and in surrender is freedom<p><p>A straight line shall break, so curve it<br>All fate lies in the bones of a spine</p>"
+    "poem": "<p>The smell of a tortoise boiling<br>It cries under the cauldron's lid</p><p>Your inheritance lies in a tomb<br>There is a past that is rightfully yours</p><p>Fear and hope created gods<br>Rid yourself of both</p>"
   },
-  sham: {
-    "title": "The Shaman",
-    "poem": "<p>Who looks outside dreams<br>Who looks inside awakes</p><p>Surrender yourself to smallness<br>and in surrender is freedom<p><p>A straight line shall break, so curve it<br>All fate lies in the bones of a spine</p>"
+  wayfarer: {
+    "title": "The Wayfarer",
+    "poem": "<p>Bring a bell for the spirits<br>and a bone for the dogs</p><p>Keep your name behind your teeth<br>Death is yet another city to see</p><p>As the sun folds time between seasons<br>you fold time with breaths</p>"
   },
-  sham: {
-    "title": "The Shaman",
-    "poem": "<p>Who looks outside dreams<br>Who looks inside awakes</p><p>Surrender yourself to smallness<br>and in surrender is freedom<p><p>A straight line shall break, so curve it<br>All fate lies in the bones of a spine</p>"
+  maker: {
+    "title": "The Maker",
+    "poem": "<p>Count grains of sand to measure the sea<br>Number the stars the measure the earth</p><p>Your dreams are myths with familiar skins<br>All you must learn has been learned before</p><p>If you do not wish for things to change<br>time shall do it for you</p>"
   }
 }
 
@@ -35,7 +36,7 @@ images.forEach(img => {
   img.addEventListener('click', (e) => {
     const target = e.target;
     const type = target.classList;
-    console.log(target)
+    console.log(type)
     images.forEach(i => {
       i.style.opacity = 0;
     })
@@ -50,17 +51,28 @@ images.forEach(img => {
     }, 250)
 
     if (type == "top") {
+      title.innerHTML = readings.conq.title;
+      poem.innerHTML = readings.conq.poem;
       //conquereor
-      console.log('hi')
-    } else if (type == "top-left") {
+    } else if (type == "left-top") {
+      title.innerHTML = readings.maker.title;
+      poem.innerHTML = readings.maker.poem;
       //maker
-    } else if (type == "top-right") {
+    } else if (type == "right-top") {
+      title.innerHTML = readings.heretic.title;
+      poem.innerHTML = readings.heretic.poem;
       //fire lol
-    } else if (type == "bottom-left") {
+    } else if (type == "left-bottom") {
+      title.innerHTML = readings.shaman.title;
+      poem.innerHTML = readings.shaman.poem;
       //shaman
-    } else if (type == "bottom-right") {
+    } else if (type == "right-bottom") {
+      title.innerHTML = readings.wayfarer.title;
+      poem.innerHTML = readings.wayfarer.poem;
       // wanderer
     } else {
+      title.innerHTML = readings.scholar.title;
+      poem.innerHTML = readings.scholar.poem;
       // scholar
     }
   });
