ab323a0fd65f4ab8e96da0cb60415b77405fe51a
c-dacanay <tina.dacanay@gmail.com>
2023-04-09 22:43:17 -0400

    99

---
+++ 99/script.js
@@ -0,0 +1,15 @@
+const compost = document.querySelector('#compost');
+const letters = compost.textContent.split('');
+
+console.log(letters)
+let dayCount = 1;
+let intervalID = setInterval(() => {
+  if (dayCount < 100) {
+    for (i = 0; i > (letters.length/100); i++ ) {
+    const randomIndex = Math.floor(Math.random() * letters.length);
+    letters[randomIndex] = `.`;
+    compost.textContent = letters.join('');
+    dayCount++;
+    }
+  }
+}, 1000 * 60 * 60 * 24); 
