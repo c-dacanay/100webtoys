cf3ef529eccb3f47d3f67f617ba428cf7a32952c
c-dacanay <tina.dacanay@gmail.com>
2023-04-10 22:44:43 -0400

    100 & edits

--- 99/script.js
+++ 99/script.js
@@ -2,7 +2,7 @@ const compost = document.querySelector('#compost');
 const letters = compost.textContent.split('');
 
 console.log(letters)
-let dayCount = 1;
+let dayCount = 0;
 let intervalID = setInterval(() => {
   if (dayCount < 100) {
     for (i = 0; i > (letters.length/100); i++ ) {
